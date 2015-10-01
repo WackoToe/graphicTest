0b5511158f053f5a55fe537939984255ff0a487d
alessandro308 <alessandropagiaro@gmail.com>
2015-10-01 23:38:22 +0200

    Tris Funzionante

--- tris_class.fsx
+++ tris_class.fsx
@@ -29,13 +29,15 @@ type Grid () as this =
 		g.RotateTransform(45.f)
 		for i = 0 to 4 do
 			g.RotateTransform(90.f)
-			g.DrawLine(Pens.Black, 0, 0, cell_size/2, 0)
+			let p = new Pen(Color.OrangeRed, Width=6.f)
+			g.DrawLine(p, 0, 0, cell_size/2, 0)
 		g.Restore(savedGraph)
 
 	member this.DrawCircle (g:Graphics) x y =
 		let savedGraph = g.Save()
 		g.TranslateTransform(float32(x*cell_size + cell_size/2), float32(y*cell_size+cell_size/2))
-		g.DrawEllipse(Pens.Black, -cell_size/2 + cell_size/20, -cell_size/2 + cell_size/20, cell_size - cell_size/20, cell_size - cell_size/20)
+		let p = new Pen(Color.CadetBlue, Width=6.f)
+		g.DrawEllipse(p, -cell_size/2 + cell_size/10, -cell_size/2 + cell_size/10, cell_size - cell_size/5, cell_size - cell_size/5)
 		g.Restore(savedGraph)
 
 	member this.CrossCircle (g:Graphics) =
@@ -70,13 +72,12 @@ type Grid () as this =
 		| y when y<cell_size -> row <-  0
 		| y when y<2*cell_size -> row <- 1
 		| _ -> row <- 2
-		stato_celle.[row, column] <- count
+		stato_celle.[row, column] <- count%2
 		count <- count+1
 		this.Invalidate()
 
 	override this.OnResize e =
 		this.Invalidate()
-
 
 let set_grid_size (e:Grid) =
 	let width = f.ClientSize.Width
